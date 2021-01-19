c468ffd971146ac20dc8b92283e4871cd54eba42
XtratoS <ultimate.nuker@yahoo.com>
2021-01-20 00:57:37 +0200

    Added middleware and implemented Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+export class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h3 className="center">Timeline</h3>
+                <ul className="center dashboard-list">
+                    {this.props.tweetsIds.map((id) => (
+                        <li key={id}>
+                            Tweet ID: {id}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({tweets}) {
+    return {
+        tweetsIds: Object.keys(tweets)
+            .sort((a, b) => (tweets[b].timestamp - tweets[a].timestamp))
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
